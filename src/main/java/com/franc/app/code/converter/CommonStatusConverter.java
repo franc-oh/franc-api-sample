600936429ed8020dd98f256599c46dbe63895683
france1988 <france1988@naver.com>
2022-08-22 13:32:33 +0900

    [서비스 개발] 멤버십 가입_개발 중   - 가입허용등급 체크 관련 테스트 코드 작성     - 코드성 데이터(enum)_<K,V> 형식으로 수정     - 데이터 조회 시 Enum 값 매핑 오류        ! Converter 구현해야 함 => 완료          - 개별 Converter는 구현 완료          - 전역 Converter 구현해야 함     ? 단순 findById가 아닌, 상태='사용' 중인 건만 노출되도록 => queryDsl 적용

--- src/main/java/com/franc/app/code/converter/CommonStatusConverter.java
+++ src/main/java/com/franc/app/code/converter/CommonStatusConverter.java
@@ -1,28 +1,12 @@
 package com.franc.app.code.converter;
 
 import com.franc.app.code.CommonStatus;
-import org.springframework.util.StringUtils;
-
-import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import java.util.EnumSet;
-import java.util.NoSuchElementException;
-import java.util.Objects;
 
 @Converter
-public class CommonStatusConverter implements AttributeConverter<CommonStatus, String> {
-    @Override
-    public String convertToDatabaseColumn(CommonStatus attribute) {
-        if(Objects.isNull(attribute)) return null;
-        return attribute.getCode();
-    }
+public class CommonStatusConverter extends CodeValueConverter<CommonStatus> {
 
-    @Override
-    public CommonStatus convertToEntityAttribute(String dbData) {
-        if(StringUtils.isEmpty(dbData)) return null;
-        return EnumSet.allOf(CommonStatus.class).stream()
-                .filter(e -> e.getCode().equals(dbData))
-                .findAny()
-                .orElseThrow(() -> new NoSuchElementException());
+    public CommonStatusConverter() {
+        super(CommonStatus.class);
     }
 }
